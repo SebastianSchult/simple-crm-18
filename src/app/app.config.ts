4b84bfafb31386c341f45b7a3cfc6a174ce945b9
Sebastian Schult <sebastianschult@MacBookAir.fritz.box>
2025-01-14 08:48:42 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-799d8","appId":"1:712496575833:web:578bcdbb0812aba1844848","storageBucket":"simple-crm-799d8.firebasestorage.app","apiKey":"AIzaSyDBme8v9hpgEeDmB1HUHDJZXncsNUZuxMs","authDomain":"simple-crm-799d8.firebaseapp.com","messagingSenderId":"712496575833"})), provideFirestore(() => getFirestore())]
 };
