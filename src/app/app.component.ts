525342fc0783e15a150ee8974f8fd09c1e8740f5
Sebastian Schult <sebastianschult@MacBookAir.fritz.box>
2025-01-14 13:52:00 +0100

    add toolbar and menu icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar';
+import {MatDrawerContainer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbar, MatToolbarModule, MatSidenavModule, MatDrawerContainer, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
