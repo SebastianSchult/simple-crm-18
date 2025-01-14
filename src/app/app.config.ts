8998c0a5ada4d5c396173d4aabc06df175c5a867
Sebastian Schult <sebastianschult@MacBookAir.fritz.box>
2025-01-14 10:29:43 +0100

    implement firestore - material - toolbar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-799d8","appId":"1:712496575833:web:578bcdbb0812aba1844848","storageBucket":"simple-crm-799d8.firebasestorage.app","apiKey":"AIzaSyDBme8v9hpgEeDmB1HUHDJZXncsNUZuxMs","authDomain":"simple-crm-799d8.firebaseapp.com","messagingSenderId":"712496575833"})), provideFirestore(() => getFirestore())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-799d8","appId":"1:712496575833:web:578bcdbb0812aba1844848","storageBucket":"simple-crm-799d8.firebasestorage.app","apiKey":"AIzaSyDBme8v9hpgEeDmB1HUHDJZXncsNUZuxMs","authDomain":"simple-crm-799d8.firebaseapp.com","messagingSenderId":"712496575833"})), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
