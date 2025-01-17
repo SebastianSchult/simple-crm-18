54ff9da82c9385ae27ffdde1602a4f494363e2bd
Sebastian Schult <sebastianschult@Sebastians-MacBook-Air.fritz.box>
2025-01-17 11:01:46 +0100

    add dialog-add-user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,15 +3,26 @@ import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatTooltipModule, TooltipPosition} from '@angular/material/tooltip';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
+import {inject} from '@angular/core';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, FormsModule, ReactiveFormsModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, FormsModule, ReactiveFormsModule, MatDialogModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
   position = new FormControl(this.positionOptions[1]);
+  readonly dialog = inject(MatDialog);
+
+  openDialog() : void {
+    this.dialog.open(DialogAddUserComponent, {
+      
+    });
+  }
 }
