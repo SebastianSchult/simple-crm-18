f316d81ac89f17e2c1e9a49166808ca508b5903f
Sebastian Schult <sebastianschult@354a0261-43c4-47d8-94e1-46952b58c522.fritz.box>
2025-01-17 10:02:20 +0100

    add add-user btn

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTooltipModule, TooltipPosition} from '@angular/material/tooltip';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, FormsModule, ReactiveFormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+  position = new FormControl(this.positionOptions[1]);
 }
