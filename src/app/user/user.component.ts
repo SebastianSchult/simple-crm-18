427d4db9a7c71c4ad3f010541d290365944eb453
Sebastian Schult <sebastianschult@MacBookAir.fritz.box>
2025-02-09 17:07:53 +0100

    fix timestamp to date issues

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatTooltipModule, TooltipPosition} from '@angular/material/tooltip';
@@ -9,19 +9,40 @@ import {inject} from '@angular/core';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 import {MatCardModule} from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, FormsModule, ReactiveFormsModule, MatDialogModule, MatCardModule], 
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, FormsModule, ReactiveFormsModule, MatDialogModule, MatCardModule, CommonModule], 
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
+  firestore: Firestore = inject(Firestore);
   user = new User();
+  allUsers: any[] = [];
   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
   position = new FormControl(this.positionOptions[1]);
   readonly dialog = inject(MatDialog);
+
+  ngOnInit(): void {
+    const usersCollection = collection(this.firestore, 'users');
+    
+    collectionData(usersCollection)
+      .subscribe((changes: any) => {
+        console.log('Changes:', changes);
+        // Wandelt den Firestore-Timestamp in ein Date-Objekt um,
+        // falls das birthDate-Objekt eine "seconds"-Eigenschaft besitzt.
+        this.allUsers = changes.map((u: any) => {
+          if (u.birthDate && typeof u.birthDate === 'object' && 'seconds' in u.birthDate) {
+            u.birthDate = new Date(u.birthDate.seconds * 1000);
+          }
+          return u;
+        });
+      });
+  }
 
   openDialog() : void {
     this.dialog.open(DialogAddUserComponent, {
