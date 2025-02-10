59102604b5901a464cb6008915aa1b53ebc16a0d
Sebastian Schult <sebastianschult@Sebastians-MacBook-Air.fritz.box>
2025-02-10 16:36:28 +0100

    fix errors in DashboardComponent

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, getCountFromServer } from '@angular/fire/firestore';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
+  firestore: Firestore;
+  totalUsers = 0;
+  currentDate: Date = new Date();
+  pendingTasks = 3;
+  private clockSubscription!: Subscription;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+              @Inject(Firestore) firestore: Firestore) {
+    this.firestore = firestore;
+  }
+
+  ngOnInit(): void {
+    this.fetchTotalUsersCount();
+
+    // Starte den Timer nur, wenn die Anwendung im Browser läuft
+    if (isPlatformBrowser(this.platformId)) {
+      this.clockSubscription = interval(1000).subscribe(() => {
+        this.currentDate = new Date();
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.clockSubscription) {
+      this.clockSubscription.unsubscribe();
+    }
+  }
+
+  async fetchTotalUsersCount() {
+    try {
+      const usersCol = collection(this.firestore, 'users');
+      const snapshot = await getCountFromServer(usersCol);
+      this.totalUsers = snapshot.data().count;
+      console.log('Total Users:', this.totalUsers);
+    } catch (error) {
+      console.error("Error fetching user count: ", error);
+    }
+  }
 }
